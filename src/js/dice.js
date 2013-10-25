705f5f7eb4378d9f75bb4b89f2365ca21d046f78
Matthias Wilhelm <mwilhelm.kl@gmail.com>
2013-10-25 19:10:19 +0200

    make the popup nicer, do I need a sort button??

--- src/js/dice.js
+++ src/js/dice.js
@@ -27,7 +27,7 @@ Dice.roll = function(num_dice, edge, sort) {
 	}
 
 	$('#dice-poptext')[0].innerHTML = "You got a "+rolls+" from "+num_dice+" dice!"+ 
-									  "<br/>This is "+count[1]+"x1 + "+count[2]+"x2 + "+count[3]+"x3 + "+count[4]+"x4 + "+count[5]+"x5 + "+count[6]+"x6 + "+
+									  "<br/>This is "+count[1]+"x1 + "+count[2]+"x2 + "+count[3]+"x3 + "+count[4]+"x4 + "+count[5]+"x5 + "+count[6]+"x6."+
 								      "<br/>This is "+hits+" hits and "+ones+" ones. "+
 								      "<br/>Edge was "+edge+" and you got "+adds+" additional hits." ;
 };

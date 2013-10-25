8a952f13edd4e442613f75646d75cc844da960e9
Matthias Wilhelm <mwilhelm.kl@gmail.com>
2013-10-25 23:27:23 +0200

    test console output v2

--- src/js/dice.js
+++ src/js/dice.js
@@ -66,7 +66,7 @@ Dice.relabel = function(active, offset) {
 	if (!active) {
 		offset = 0;
 	}
-	console.log($('.dicebutton')[0]);
+	console.log($('.dicebutton')[0].baseval);
 
 	$('.dicebutton').each(
 		function(index) {

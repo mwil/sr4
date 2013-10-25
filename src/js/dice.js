6a5af16e2157305b658c9f97e7767f41c9f731c3
Matthias Wilhelm <mwilhelm.kl@gmail.com>
2013-10-25 18:43:55 +0200

    starting to add edge, now someting happens

--- src/js/dice.js
+++ src/js/dice.js
@@ -1,24 +1,25 @@
 var Dice = {};
 
 Dice.roll = function(num_dice, edge) {
-	var rolls = new Array(num_dice);
+	var rolls = new Array();
 	var hits  = 0;
 	var ones  = 0;
-
-	if (edge) {
-		alert(edge);
-	}
+	var adds  = 0;
 	
-	
-	for (var i = 0; i < num_dice; i++) {
+	for (var i = 0; i < (num_dice + adds); i++) {
 		rolls[i] = 1 + Math.floor(Math.random() * 6);
 		if (rolls[i] > 4) {
 			hits += 1;
 		} else if (rolls[i] == 1) {
 			ones += 1;
 		}
+
+		if (edge and rolls[i] == 6) {
+			adds += 1;
+		}
 	};
 
 	$('#dice-poptext')[0].innerHTML = "You got a " + rolls + " from " + num_dice + " dice!" + 
-								      "</br>This is " + hits + " hits and " + ones + " ones.";
+								      "<br/>This is " + hits + " hits and "+ ones + " ones. "+
+								      "<br/>Edge was "+ edge + "and you got "+ adds+" additional hits." ;
 };
